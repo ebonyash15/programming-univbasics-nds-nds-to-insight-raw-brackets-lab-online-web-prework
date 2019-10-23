c37217fc8a392fd7629d73fd0fdb75000b26e7df
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-23 03:19:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,16 +8,11 @@ def directors_totals(nds)
     cumulative = 0
     title_index = 0
     director_name = nds[director_index][:name]
-    p "cumulative title_index director_name"
-    p cumulative
-    p director_name
     while title_index < nds[director_index][:movies].length do
       cumulative += nds[director_index][:movies][title_index][:worldwide_gross]
       title_index += 1
       result[director_name] = cumulative
-      p result
     end
     director_index += 1
   end
-  p result
 end
